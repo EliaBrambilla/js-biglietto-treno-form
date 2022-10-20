5f1e5e7cb5908509951a32601a6b7515262a5d8b
EliaBrambilla <elia.brambilla@icloud.com>
2022-10-20 15:15:27 +0200

    aggiungo variabili js

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+/* variabili */
+let passengerFullName = document.getElementById("nomeCognome");
+let km = document.getElementById("km");
+let ageRange = document.getElementById("fasciaEta");
+
+let calcola = document.getElementById("calcola");
+let annulla = document.getElementById("annulla");
+
+let discountJunior = 0.20;
+let discountSenior = 0.40;
+let ticketRate = 0.21;
+let outputPrice;
+let totalPrice = 0;
